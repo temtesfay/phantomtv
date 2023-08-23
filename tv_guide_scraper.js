9048886f6ff37e5f7ad633d15c4d2d606aa72b78
temtesfay~~ <temtesfay1234@gmail.com>
2023-08-23 17:09:08 -0600

    Update tv_guide_scraper.js

--- tv_guide_scraper.js
+++ tv_guide_scraper.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { title } = require('process');
 
 const app = express();
-const port = 4000;
+const port = 5000;
 
 const url = 'https://tv-guide-listings.co.uk/';
 const SerieAurl = 'https://www.live-footballontv.com/live-italian-football-on-tv.html'
@@ -40,6 +40,49 @@ async function scrapeData(channel) {
     throw error;
   }
 }
+
+// Cache object to store scraped data
+const cache = {};
+
+// Function to scrape data
+// async function scrapeData(channel) {
+//   try {
+//     if (cache[channel] && cache[channel].expiration > Date.now()) {
+//       // If data is already in cache and not expired, return it
+//       return cache[channel].data;
+//     }
+
+//     const response = await axios.get(url);
+//     if (response.status === 200) {
+//       const $ = cheerio.load(response.data);
+//       const btSport1Element = $(channel);
+
+//       // Extract data...
+//       // Extract data for cell1 component
+//       const cell1TitleElement = btSport1Element.find('.timeline-item.cell1 .title');
+//       const cell1Title = cell1TitleElement.clone().children().remove().end().text().trim();
+//       const cell1Description = cell1TitleElement.find('.sDesc').text().trim();
+//       const cell1Time = (btSport1Element.find('.timeline-item.cell1 .time').text().trim() + " " + btSport1Element.find('.timeline-item.cell2 .time').text().trim()).slice(0, -1);
+//       const progressTime = btSport1Element.find('.progress span').attr('style').slice(6, -2);
+       
+  
+//       // Return the scraped data as an object
+//       const scrapedData = { title: cell1Title, description: cell1Description, time: cell1Time, progress: progressTime };
+
+     
+//       // Store scraped data in cache with expiration time (e.g., 30 seconds from now)
+//       cache[channel] = { data: scrapedData, expiration: Date.now() + 1 * 30 * 1000 };
+
+//       return scrapedData;
+//     } else {
+//       console.log(`Failed to fetch the data. Status code: ${response.status}`);
+//       throw new Error('Failed to fetch data');
+//     }
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//     throw error;
+//   }
+// }
 
 
 
